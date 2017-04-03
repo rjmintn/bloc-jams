2175310b26dba098fc5565943b2737092f88d2ec
Robert Morawski <rjmorawski@yahoo.com>
2017-04-02 20:54:04 -0400

    Checkpoint 9 dom scripting events complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,29 @@
+var pointsArray = document.getElementsByClassName('point');
 
-
-var animatePoints = function () {
+var animatePoints = function (points) {
     
-    var points = document.getElementsByClassName('point');
-
-    function revealPoint(pointsIndex) {
+    var revealPoint = function (pointsIndex) {
         points[pointsIndex].style.opacity = 1;
         points[pointsIndex].style.transform = "scaleX(1) translateY(0)";
         points[pointsIndex].style.msTransform = "scaleX(1) translateY(0)";
         points[pointsIndex].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+    };
 
     for (var i=0; i<points.length; i++){
         revealPoint(i);
     };
 
 };
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight +200;
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
